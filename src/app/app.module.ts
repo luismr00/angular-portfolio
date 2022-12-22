f1490ab92057a0199cd9635ad5e9a6d9f371aa8e
Luis Rangel <luismrangel7@gmail.com>
2022-12-21 20:01:25 -0600

    initial progress bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { AboutComponent } from './components/about/about.component';
 import { WorkComponent } from './components/work/work.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SkillsComponent } from './components/skills/skills.component';
+
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -20,11 +23,44 @@ import { HeaderComponent } from './components/header/header.component';
     AboutComponent,
     WorkComponent,
     ContactComponent,
-    HeaderComponent
+    HeaderComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Specify ng-circle-progress as an import
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      // radius: 100,
+      // outerStrokeWidth: 16,
+      // innerStrokeWidth: 8,
+      // outerStrokeColor: "#78C000",
+      // innerStrokeColor: "#C7E596",
+      // animationDuration: 300,
+
+      radius: 60,
+      space: -10,
+      outerStrokeGradient: true,
+      outerStrokeWidth: 10,
+      outerStrokeColor: "#4882c2",
+      outerStrokeGradientStopColor: "#53a9ff",
+      innerStrokeColor: "#e7e8ea",
+      innerStrokeWidth: 10,
+      // title: "auto",
+      // subtitle: "Percent",
+      titleColor: 'white',
+      subtitleFontSize: '20',
+      subtitleColor: 'white',
+      animateTitle: false,
+      animationDuration: 1000,
+      showUnits: false,
+      showBackground: false,
+      clockwise: false,
+      startFromZero: false,
+      lazy: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
